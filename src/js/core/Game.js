b88c3e5c73a3c2f9fcb6047009ca22df3ec2a606
a-ghost-fart <a-ghost-fart@fedora.sexy>
2015-01-05 20:42:54 +0000

    arm

--- src/js/core/Game.js
+++ src/js/core/Game.js
@@ -19,7 +19,7 @@ function Game() {
     this.context = this.canvas.getContext('2d');
     document.body.appendChild(this.canvas);
 
-    this.input = new Input();
+    this.input = new Input(this.canvas);
 
     this.game_loop();
 }
@@ -45,6 +45,37 @@ Game.prototype.draw = function () {
     'use strict';
 
     this.clear_canvas();
+
+    var start = this.player.position;
+    var mouse = this.input.get_mouse_position();
+
+    var xs = 0;
+    var ys = 0;
+    xs = mouse.x - start.x;
+    xs = xs * xs;
+    ys = mouse.y - start.y;
+    ys = ys * ys;
+    var how_far = Math.floor(Math.sqrt(xs + ys));
+
+    var angle = Math.atan2(mouse.x - start.x, mouse.y - start.y);
+    var steps = Math.floor(how_far / 30);
+    var distance = how_far / steps;
+    var sin = Math.sin(angle) * distance;
+    var cos = Math.cos(angle) * distance;
+
+    var img = new Image();
+    img.src = '/assets/arm.png';
+
+    this.context.strokeStyle = '#fff';
+
+    for (var i = 1; i <= steps; i++) {
+        this.context.save();
+        this.context.translate(start.x + (i * sin), start.y + (i * cos));
+        this.context.rotate(-angle);
+        this.context.drawImage(img, 0, 0);
+        this.context.restore();
+    }
+
 
     for (var entity in this.entities) {
         if (this.entities.hasOwnProperty(entity)) {
--- src/js/util/Input.js
+++ src/js/util/Input.js
@@ -1,10 +1,25 @@
-function Input() {
+var Point = require('./Point');
+
+function Input(canvas) {
     'use strict';
     this.pressed = [];
+    this.mouse_location = new Point(0, 0);
 
+    canvas.addEventListener('mousemove', this.mouse_move.bind(this), false);
     document.addEventListener('keyup', this.keyup.bind(this), false);
     document.addEventListener('keydown', this.keydown.bind(this), false);
 }
+
+Input.prototype.mouse_move = function (event) {
+    'use strict';
+    this.mouse_location.x = event.clientX;
+    this.mouse_location.y = event.clientY;
+};
+
+Input.prototype.get_mouse_position = function () {
+    'use strict';
+    return this.mouse_location;
+};
 
 Input.prototype.is_key_down = function (keycode) {
     'use strict';
